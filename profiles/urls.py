7b72da86693e4be4176068129f6271bee6c12f0c
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-10-01 22:08:32 +0300

    added allauth templates

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from . import views
 
+app_name = 'profiles'
 
 urlpatterns = [
     path(r'',views.dashboard,name='dashboard'),
+    path(r'change_mode/',views.change_user_mode,name='change_mode'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -1,13 +1,37 @@
 from django.shortcuts import render,redirect
 from .models import UserProfile
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
+from shop.models import Cart, Wishlist
 
 # Create your views here.
 @login_required()
 def dashboard(request):
     user_profile = UserProfile.objects.get(user=request.user)
+    cart = Cart.objects.get(user=user_profile)
+    wishlist = Wishlist.objects.get(user=user_profile)
 
     context = {
-        'user_profile':user_profile
+        'user_profile':user_profile,
+        'cart':cart,
+        'wishlist':wishlist
     }
     return render(request, "profiles/dashboard.html",context)
+
+
+# For now, the process for changing the user mode will be hidden inside the dashbord
+# When we merge react frontend, it will be a button accesible on the drop-down Menu
+@login_required()
+def change_user_mode(request):
+    user_profile = UserProfile.objects.get(user=request.user)
+
+    if user_profile.mode == 'S':
+        user_profile.mode = 'B'
+        user_profile.save()
+        messages.success(request, "You have successfully switched to Buyer Mode")
+        return redirect('profiles:dashboard')
+    else:
+        user_profile.mode = 'S'
+        user_profile.save()
+        messages.success(request, "You have successfully switched to Seller Mode")
+        return redirect('profiles:dashboard')
