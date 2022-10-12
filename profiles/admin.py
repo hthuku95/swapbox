7bdcf0fe8671a3dbbc8fcbf1a91102fe8ab74961
HARRY OMOLLOH <harryomollo95@gmail.com>
2022-10-12 10:23:25 +0300

    Registered wallet and profile settings on Admin

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile,Wallet,ProfileSettings
 # Register your models here.
 
 admin.site.register(UserProfile)
+admin.site.register(Wallet)
+admin.site.register(ProfileSettings)
