2b8d141d08fbb035be1523f886ac4e127b46a86c
HARRY OMOLLOH <hthuku95@gmail.com>
2023-05-20 16:11:56 +0300

    Added payments and checkout urls

--- payments/urls.py
+++ payments/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'payments'
 
 urlpatterns = [
-    path(r'payment/<int:id>/',views.payment_details,name='payment-details'),
+    path(r'payment/<slug>/',views.payment_details,name='payment-details'),
+    path(r'checkout/',views.checkout_view,name='checkout-view'),
 ]
