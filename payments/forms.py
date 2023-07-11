9d66766043b3b10a6297f39c29b367b10b3bf81f
HARRY OMOLLOH <hthuku95@gmail.com>
2023-07-11 13:07:09 +0300

    Added payment method choices

--- payments/forms.py
+++ payments/forms.py
@@ -1,10 +1,10 @@
 from django import forms
 
-PAYMENT_METHOD_CHOICES = [
-
-]
-
 class CheckoutForm(forms.Form):
+    PAYMENT_METHOD_CHOICES = (
+        ('PC','Paypal or Card'),
+        ('W','Wallet')
+    )
     first_name = forms.CharField(required=False)
     last_name = forms.CharField(required=False)
     billing_address = forms.CharField(required=False)
@@ -12,7 +12,5 @@ class CheckoutForm(forms.Form):
     billing_zip = forms.CharField(required=False)
     set_default_billing = forms.BooleanField(required=False)
     use_default_billing = forms.BooleanField(required=False)
-
-# Add radio button for selecting payment method
-class PaymentMethodForm(forms.Form):
-    payment_method = forms.ComboField()
+    
+    payment_method = forms.ChoiceField(widget=forms.RadioSelect, choices=PAYMENT_METHOD_CHOICES)
